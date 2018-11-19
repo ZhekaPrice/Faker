c7ecaa4dc1ead15626f287b747170404c7f9d91b
Zhecha <j.prejs@yandex.com>
2018-11-20 02:05:44 +0300

    add class with private constructor for testing Faker

---
+++ Faker/ExampleUseFaker/PrivateClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUseFaker
+{
+    public class PrivateClass
+    {
+        public int Age;
+        public string Name;
+        public bool IsTrue;
+
+        private PrivateClass(int age, string name, bool istrue)
+        {
+            Age = age;
+            Name = name;
+            IsTrue = istrue;
+        }
+    }
+}
