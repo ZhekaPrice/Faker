83bb2bf03d98df3067bc921e83bd6e9a6f4defb8
Zhecha <j.prejs@yandex.com>
2018-11-19 23:16:28 +0300

    add Bar class for testing Faker

---
+++ Faker/ExampleUseFaker/Bar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUseFaker
+{
+    public class Bar
+    {
+        public string Str;
+        public double NewDouble;
+        public List<int> IntList;
+        public DateTime DateTime;
+        public char Sumbol2;
+        public DateTime DateTime1;
+
+        public Bar(string str, double newDouble, List<int> intList, DateTime dateTime)
+        {
+            Str = str;
+            NewDouble = newDouble;
+            IntList = intList;
+            DateTime = dateTime;
+        }
+
+    }
+}
