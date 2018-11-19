96b33473068d2db6a2a66ecd9ccd1e55b6c5ea2f
Zhecha <j.prejs@yandex.com>
2018-11-19 22:42:51 +0300

    add plugin for List

---
+++ Faker/ListPlugin/ListGeneratorPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+
+namespace ListGeneratorPlugin
+{
+    public class ListGeneratorPlugin : IPlugin
+    {
+        public Type type
+        {
+            get { return typeof(List<>); }
+        }
+
+        public object GenerateRandomValue(Type type)
+        {
+            return (IList)Activator.CreateInstance(type);
+        }
+    }
+}
