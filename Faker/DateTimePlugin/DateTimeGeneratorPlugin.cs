c4470400c6633c3cae1a5858e29a0f8f697fd739
Zhecha <j.prejs@yandex.com>
2018-11-19 22:11:26 +0300

    add plugin for DateTime

---
+++ Faker/DateTimePlugin/DateTimeGeneratorPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+
+namespace DateTimeGeneratorPlugin
+{
+    public class DateTimeGeneratorPlugin : IPlugin
+    {
+        private Random random;
+
+        public Type type
+        {
+            get { return typeof(DateTime); }
+        }
+
+        public DateTimeGeneratorPlugin()
+        {
+            random = new Random();
+        }
+
+        public object GenerateRandomValue(Type type)
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
+        }
+    }
+}
