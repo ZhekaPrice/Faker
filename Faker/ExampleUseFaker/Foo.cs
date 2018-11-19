b54dee2f8e89ac848137fdfcb44b12f4505ae3c5
Zhecha <j.prejs@yandex.com>
2018-11-20 01:30:25 +0300

    add class Foo for testing Faker

---
+++ Faker/ExampleUseFaker/Foo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUseFaker
+{
+    public class Foo
+    {
+        public int Age;
+        public string Name;
+        public bool IsTrue;
+        public Bar Bar;
+        public float Float;
+        public long Long;
+        public string Str1;
+
+        public Foo(int age, string name, bool isTrue, Bar bar)
+        {
+            Age = age;
+            Name = name;
+            IsTrue = isTrue;
+            Bar = bar;
+        }
+    }
+}
