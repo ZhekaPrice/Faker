ea34d2eea169331ceac30fb270ece6cb8e2f15fb
Zhecha <j.prejs@yandex.com>
2018-11-20 04:02:09 +0300

    add class generation double values

---
+++ Faker/Faker/BasicDoubleGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicDoubleGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicDoubleGenerator()
+        {
+            random = new Random();
+        }
+
+        public object GenerateRandomValue()
+        {
+            return random.NextDouble();
+        }
+    }
+}
