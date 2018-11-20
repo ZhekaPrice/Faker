566f51eb9f3360f95de673a3d0626a88be9c8b0f
Zhecha <j.prejs@yandex.com>
2018-11-20 05:24:18 +0300

    add class generation long values

---
+++ Faker/Faker/BasicLongGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicLongGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicLongGenerator()
+        {
+            random = new Random();
+        }
+        public object GenerateRandomValue()
+        {
+            return random.Next();
+        }
+    }
+}
