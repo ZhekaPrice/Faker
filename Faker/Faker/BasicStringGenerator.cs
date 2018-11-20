4ceb0e7f62b2cd07da2b38d14656bfbc5fb0cae8
Zhecha <j.prejs@yandex.com>
2018-11-20 05:38:39 +0300

    add class generation string values

---
+++ Faker/Faker/BasicStringGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicStringGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicStringGenerator(){
+            random = new Random();
+        }
+
+        public object GenerateRandomValue()
+        {
+            string[] stringArray = { "MI:Chost Protocol", "MI:Rogue Nation", "MI:FallOut" };
+            return stringArray[random.Next(0,3)];
+        }
+    }
+}
