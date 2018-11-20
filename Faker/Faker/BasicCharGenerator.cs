0a76b44a61a5473fde1201b1b644d7046b9cae11
Zhecha <j.prejs@yandex.com>
2018-11-20 03:35:27 +0300

    add class generation Char's values

---
+++ Faker/Faker/BasicCharGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicCharGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicCharGenerator()
+        {
+            random = new Random();
+        }
+
+        public object GenerateRandomValue()
+        {
+            var charString = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm";
+            return charString[random.Next(charString.Length)];
+        }
+    }
+}
