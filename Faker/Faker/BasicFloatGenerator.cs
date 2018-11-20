a1b97930db36b5cff41bdeb2fdeb2be635ae2ad7
Zhecha <j.prejs@yandex.com>
2018-11-20 04:08:33 +0300

    add class generation float values

---
+++ Faker/Faker/BasicFloatGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicFloatGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicFloatGenerator()
+        {
+            random = new Random();
+        }
+        public object GenerateRandomValue()
+        {
+            return random.Next();
+        }
+    }
+}
