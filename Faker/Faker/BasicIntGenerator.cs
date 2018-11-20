b151862f7ab3941565d0103dd1ab3c947e421f4c
Zhecha <j.prejs@yandex.com>
2018-11-20 04:26:55 +0300

    add class generation int values

---
+++ Faker/Faker/BasicIntGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicIntGenerator : IBasicGenerator
+    {
+        private Random random;
+
+        public BasicIntGenerator()
+        {
+            random = new Random();
+        }
+        public object GenerateRandomValue()
+        {
+            return random.Next();
+        }
+    }
+}
