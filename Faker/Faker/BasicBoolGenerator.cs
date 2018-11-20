0d3b8ca18d76c423808cf1b159e801243371d0b3
Zhecha <j.prejs@yandex.com>
2018-11-20 03:27:51 +0300

    add class generation bool values

---
+++ Faker/Faker/BasicBoolGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class BasicBoolGenerator : IBasicGenerator
+    {
+        public object GenerateRandomValue()
+        {
+            return true;
+        }
+    }
+}
