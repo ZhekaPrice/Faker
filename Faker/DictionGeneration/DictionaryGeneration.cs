f0f907d2f4f598b52d26ffbc245c72ea85e53290
Zhecha <j.prejs@yandex.com>
2018-11-22 16:52:48 +0300

    add plugin for Dictionary generation

---
+++ Faker/DictionGeneration/DictionaryGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+
+namespace DictionGeneration
+{
+    public class DictionaryGeneration : IPlugin
+    {
+
+        public Type type
+        {
+            get { return typeof(Dictionary<,>); }
+        }
+
+        public object GenerateRandomValue(Type type)
+        {
+            return (IDictionary)Activator.CreateInstance(type);
+        }
+    }
+}
